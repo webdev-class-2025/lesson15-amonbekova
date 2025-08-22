2833bd91fc4d40d8bf50591c3fa38cc26328c5dd
Zuhro Amonbekova <zuhroamonbekova148@gmail.com>
2025-08-22 14:48:34 +0500

    myhomework

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+let title = "Наушники";
+let price = 1500;      
+let discount = 10;    
+let rating = 4.5;      
+let inStock = true;    
 
+
+let finalPrice = price - (price * discount / 100);
+
+
+let stockText = inStock ? "В наличии" : "Нет в наличии";
+
+console.log(`Название: ${title}`);
+console.log(`Цена без скидки: ${price}`);
+console.log(`Скидка: ${discount}%`);
+console.log(`Итоговая цена: ${finalPrice}`);
+console.log(`Рейтинг: ${rating}/5`);
+console.log(`Наличие: ${stockText}`);
+
+console.log("Типы (typeof):");
+console.log("title →", typeof title);
+console.log("price →", typeof price);
+console.log("discount →", typeof discount);
+console.log("rating →", typeof rating);
+console.log("inStock →", typeof inStock);
